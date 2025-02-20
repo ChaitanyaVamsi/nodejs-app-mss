ad6f0cbd747b5574f15a4e131064222ec7c9d2bf
ChaitanyaVamsi <45822767+ChaitanyaVamsi@users.noreply.github.com>
2025-02-20 20:53:16 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://localhost:9000/',
+  serverUrl: 'http://localhost:9000',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'sqa_7927c58e3f263778f7748b4982ac50900ff8454a',
+	    'sonar.login': 'sqa_a8ca9d891401079ccaa9b8af4ee50b9c30622dc3',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
